27409d7ff079331ff53b8660ee4db8c33799fa65
Emma May <emmamay@Emmas-MacBook-Pro.local>
2025-01-05 19:51:09 -0700

    [initial-commit] followed readme to get app running, added packages for auth w google

--- Client/Program.cs
+++ Client/Program.cs
@@ -8,4 +8,15 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
+// var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+
+services.AddAuthentication().AddGoogle(googleOptions =>
+    {
+        googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
+        googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+    });
+
+
 await builder.Build().RunAsync();
